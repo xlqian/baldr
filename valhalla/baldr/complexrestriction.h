c0cc141a425198f175fc67a16a9cdefa6384a04c
gknisely <greg@mapzen.com>
2016-08-22 16:37:35 -0400

    added comments.

--- valhalla/baldr/complexrestriction.h
+++ valhalla/baldr/complexrestriction.h
@@ -28,7 +28,8 @@ class ComplexRestriction {
 
   /**
    * Constructor
-   * @param  ptr  Pointer to a bit of memory that has the info for this complex restriction
+   * @param   ptr  Pointer to a bit of memory that has the info for this complex restriction
+   * @param   reverse - should we reverse the vias?
    */
   ComplexRestriction(char* ptr, const bool reverse = false);
 
@@ -37,45 +38,91 @@ class ComplexRestriction {
    */
   virtual ~ComplexRestriction();
 
-  // restriction is from this id
+  /**
+   * Get the restriction's from id
+   * @return  Returns the from id
+   */
   uint64_t from_id() const;
 
-  // restriction is to this id
+  /**
+   * Get the restriction's to id
+   * @return  Returns the to id
+   */
   uint64_t to_id() const;
 
-  // Get the reversed flag
+  /**
+   * Get the reversed flag
+   * @return  Returns the reversed flag.
+   */
   bool is_reversed() const;
 
-  // Get the number of vias.
+  /**
+   * Get the number of vias.
+   * @return  Returns the number of vias
+   */
   uint64_t via_count() const;
 
-  // Get the via based on the index.
+  /**
+   * Get the via id at a index
+   * @param  index  Index into the via list.
+   * @return  Returns the via
+   */
   uint64_t GetViaId(uint8_t index) const;
 
-  // Get the type
+  /**
+   * Get the restriction type
+   * @return  Returns the restriction type
+   */
   RestrictionType type() const;
 
-  // Get the modes impacted by the restriction.
+  /**
+   * Get the modes impacted by the restriction.
+   * @return  Returns the access modes
+   */
   uint64_t modes() const;
 
-  // Get the begin day for this restriction.
+  /**
+   * Gets the begin day for this restriction.
+   * @return  Returns the begin day
+   */
   DOW begin_day() const;
 
-  // Get the end day for this restriction.
+  /**
+   * Gets the end day for this restriction.
+   * @return  Returns the end day
+   */
   DOW end_day() const;
 
-  // Gets the begin time for this restriction.
+  /**
+   * Gets the begin time for this restriction.
+   * @return  Returns the begin time in seconds.
+   */
   uint64_t begin_time() const;
 
-  // Gets the end time for this restriction.
+  /**
+   * Gets the end time for this restriction.
+   * @return  Returns the end time in seconds.
+   */
   uint64_t end_time() const;
 
-  // Gets the elapsed time for this restriction.
+  /**
+   * Gets the elapsed time for this restriction.
+   * @return  Returns the elapsed time in seconds.
+   */
   uint64_t elapsed_time() const;
 
-  // Get a list of names
+  /**
+   * Get a list of vias
+   * @return  Returns the list of vias
+   */
   const std::vector<uint64_t> GetVias() const;
 
+  /**
+   * overloaded == operator - used to ensure no dups in tiles.
+   * @param  other  ComplexRestriction to compare to.
+   * @return  Returns true or false if equal or not.
+   *
+   */
   bool operator == (const ComplexRestriction& other) const;
 
   struct PackedRestriction {
